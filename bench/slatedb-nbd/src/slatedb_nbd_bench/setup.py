e1aa04ae5a602c0dca6af6e30cb824f69cd4c183
John Parton <john.parton.iv@gmail.com>
2025-08-08 23:37:27 -0500

    Setup.py

---
+++ bench/slatedb-nbd/src/slatedb_nbd_bench/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="slatedb_nbd_bench",
+    version="0.1.0",
+    description="Benchmarking tool for SlateDB NBD projects",
+    packages=["slatedb_nbd_bench"],
+    package_dir={"": "src"},
+)
